6a8063579534ab0a833f21cd7da503ef3564bb3b
shubham bagherwal <shubham.bagherwal@oaktreecloud.com>
2022-04-13 20:03:12 +0530

    migration change

--- db/migrate/20220307105714_remove_from_comments.rb
+++ db/migrate/20220307105714_remove_from_comments.rb
@@ -1,8 +1,8 @@
 class RemoveFromComments < ActiveRecord::Migration[7.0]
   def change
     remove_column :comments, :blog_id
-    remove_column :comments, :user_id
-    remove_column :comments, :relation_id
+    #remove_column :comments, :user_id
+    #remove_column :comments, :relation_id
     
   end
 end
